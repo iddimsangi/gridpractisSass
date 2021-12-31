50edbebe05e992b309c11bdd9b80246214428b5d
iddimsangi <adammsangi@gmail.com>
2021-12-31 09:17:12 +0300

    Structure Layout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="AppContainer">
+        <nav className="AppContainer-nav">
+          Nav Header
+        </nav>
+        <sidebar className="AppContainer-item1">
+          Side Bar
+        </sidebar>
+        <div className="AppContainer-item2">Item One</div>
+        <div className="AppContainer-item3">Item Two</div>
+        <div className="AppContainer-item4">Item Three</div>
+        <div className="AppContainer-item5">Item Four</div>
+        <div className="AppContainer-item6">Item Five</div>
+        <div className="AppContainer-item7">Item Six</div>
+        <footer className="AppContainer-itemFooter">Footer</footer>
     </div>
   );
 }
